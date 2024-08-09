8eaa4a0e0a56d71e734bf092978460d1a8bb7eaa
Darren Bellew <db.darren6546@gmail.com>
2024-08-09 14:40:00 +0100

    added input
    Added an input for how many cards to generate

--- PackOpener.py
+++ PackOpener.py
@@ -1,15 +1,14 @@
 import mymodule
 
-
-howMany = 10
+howMany = int(input("How many cards do you want to make?: "))
 setOfCards = []
 
 for x in range(howMany):
-    setOfCards.append(mymodule.Card("Card " + str(x)))
+    setOfCards.append(mymodule.Card("Card " + str(x+1)))
 
 for x in setOfCards:
     print(x.name + ": " + str(x.rarity))
 
 customRare = mymodule.Card("This is a Rare Card: ", mymodule.rare[0])
 
-print(customRare.name + ": " + str(customRare.rarity))
+#print(customRare.name + ": " + str(customRare.rarity))
